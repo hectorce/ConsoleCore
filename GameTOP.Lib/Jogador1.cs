2d51636d6e32970efdb8a3886fdcb8adaa08d0ee
Hector Holanda <hectorce@gmail.com>
2021-09-27 19:38:35 -0300

    first commit

---
+++ GameTOP.Lib/Jogador1.cs
@@ -0,0 +1,28 @@
+using GameTOP.Interface;
+
+namespace GameTOP.Lib
+{
+ public class Jogador1 : iJogador
+    {
+        public readonly string _Nome;
+        
+        public Jogador1(string nome = "Hector")
+        {
+            _Nome = nome;
+        }
+        public string Chuta()
+        {
+            return $"{_Nome} está Chutando \n";
+        }
+
+        public string Corre()
+        {
+            return $"{_Nome} está Correndo \n";
+        }
+
+        public string Passe()
+        {
+            return $"{_Nome} está Passando \n";
+        }
+    }
+}
